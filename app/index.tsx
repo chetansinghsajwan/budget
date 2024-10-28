1367beb41219a63848a08ea945fdf5c25965e681
shifu-dev <csingh8476@gmail.com>
2024-10-28 13:00:21 +0530

    update: add theme support

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { View } from '@components/ui/View'
 import { TransactionsPage } from './Transactions'
+import { ThemeProvider } from '@react-navigation/native'
+import { useColorScheme } from 'react-native'
+import { darkTheme, lightTheme } from '@components/theme'
+import { View } from '@components/ui/View'
 
-export default function HomeScreen() {
+export const App = () => {
+  const colorScheme = useColorScheme()
+  const theme = colorScheme === 'light' ? lightTheme : darkTheme
+
   return (
-    <View
-      style={{
-        height: '100%',
-        width: '100%',
-      }}
-    >
-      <TransactionsPage />
-    </View>
+    <ThemeProvider value={theme}>
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <TransactionsPage />
+      </View>
+    </ThemeProvider>
   )
 }
+
+export default App
